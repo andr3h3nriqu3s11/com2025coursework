092cbe3435699c8a36effcc8924311c5ffdcc61f
Andre Henriques <ag01598@surrey.ac.uk>
2021-11-18 17:39:25 +0000

    Update to contact page and header

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'contact/contact'
   root 'home#home'
+  get 'contact' => 'contact#contact'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
