daefca0878c69f0d774b570885b7d19904a3d762
Andre Henriques <ag01598@surrey.ac.uk>
2021-11-27 15:41:09 +0000

    Setup mailer preview

--- test/mailers/previews/contact_mailer_preview.rb
+++ test/mailers/previews/contact_mailer_preview.rb
@@ -1,4 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/contact_mailer
 class ContactMailerPreview < ActionMailer::Preview
 
+  def contact_email
+    ContactMailer.contact_email("test@andr3h3nriqu3s.com", "Tester", "This is a test", "Of the preview functionality")
+  end
 end
