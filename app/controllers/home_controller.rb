8eee528eec10b56e627d09f7789cc9848aa5520b
Andre Henriques <ag01598@surrey.ac.uk>
2021-11-29 23:48:04 +0000

    Started working on the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,10 @@ class HomeController < ApplicationController
     if user_signed_in?
       redirect_to dashboard_path
     end
+
+    @wallet = Wallet.new
+    @wallet.name = "This is a wallet"
+
   end
 
 end
