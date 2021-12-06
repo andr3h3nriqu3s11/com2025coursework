6c45312c84ca3163472fb33f426f511bb654a275
Goncalves Henriques <ag01598@heron011.eps.surrey.ac.uk>
2021-12-06 13:27:43 +0000

    Update to run_lab.sh added the seed option

--- run_lab.sh
+++ run_lab.sh
@@ -2,10 +2,15 @@ export BUNDLE_PATH=".gems"
 
 echo -e "\n\n\n\ninstall\n\n\n\n"
 bundle install
-echo -e "\n\n\n\nmigrate\n\n\n\n"
-rails db:migrate
-echo -e "\n\n\n\nseed\n\n\n\n"
-rails db:seed
+if [ "$1" = "seed" ] || [ ! -f seeded ]; then
+	echo -e "\n\n\n\npurging\n\n\n\n"
+	rails db:purge
+	echo -e "\n\n\n\nmigrate\n\n\n\n"
+	rails db:migrate
+	echo -e "\n\n\n\nseed\n\n\n\n"
+	rails db:seed
+	touch seeded
+fi
 echo -e "\n\n\n\nrun the server\n\n\n\n"
 rails s
 
